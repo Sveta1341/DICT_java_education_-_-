e67a5c612edd38dd40a2a000656975ddb6828bb5
Sveta1341 <bsvetik1341@gmail.com>
2021-10-04 12:37:31 +0300

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,10 @@
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main (String[] args {
         System.out.println("Java education");
+        //new string
+        System.out.println("DICT");
+
     }
 }
 
