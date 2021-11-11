88eaba1c367569242a3fb1f7135d00ad30f75392
Sveta1341 <bsvetik1341@gmail.com>
2021-11-11 18:31:11 +0200

    ChatBot 2 stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,4 +1,9 @@
 package ChatBot;
+import java.util.Scanner;
+
+
+
+
 
 public class ChatBot{
     public String name;
@@ -9,14 +14,20 @@ public class ChatBot{
         year = numYear;
     }
 
+
     public void printBot(){
         System.out.println("Hello! My name is " + name );
         System.out.println("I was created in " + year );
+        System.out.println("Please, remind me your name." );
 
     }
 
     public static void main(String args[]){
         ChatBot empOne = new ChatBot("Aboba", 2021);
         empOne.printBot();
+        Scanner in = new Scanner(System.in);
+        String num = in.nextLine();
+        System.out.printf("What a great name you have, %s ", num + "!");
+        in.close();
     }
 }
