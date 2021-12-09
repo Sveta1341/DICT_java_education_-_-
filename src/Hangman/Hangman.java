cd24969c44d418c07f29c7c5f366bd9e5b39fb59
Sveta1341 <bsvetik1341@gmail.com>
2021-12-09 19:33:17 +0200

    Hangman 1 stage

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class Hangman {
+    public static void main(String args[]) {
+        System.out.println("HANGMAN\n");
+        System.out.println("The game will be available soon.");
+    }
+}
